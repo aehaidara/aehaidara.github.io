2cb14cb937a2c15ec3b86f032f835f73bcb10317
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-11-10 23:23:02 +0100

    fixed cookie consent banners

--- docs/js/components/footer.js
+++ docs/js/components/footer.js
@@ -65,13 +65,17 @@ class Footer extends HTMLElement {
             </div>
 
             <p class="footer__copywrite">
-                Copyright &copy; <span id="copyrightYear"></span> DARA
-                Coaching |
+                
 
                 <!-- Below is the link that users can use to open Preferences Center to change 
                 their preferences. Do not modify the ID parameter. Place it where appropriate, 
                 style it as needed. -->
-                <a class="underline" href="#" id="open_preferences_center">Update cookies preferences</a>
+                <a class="underline" href="./cookie-policy.html" id="open_preferences_center">Cookie</a>
+                & <a class="underline" href="./privacy-policy.html" id="open_preferences_center">Privacy</a> Policies
+                | <a class="underline" href="#" id="open_preferences_center">Update cookies preferences</a>
+                <br>
+                Copyright &copy; <span id="copyrightYear"></span> DARA
+                Coaching
             </p>
 
       `;
